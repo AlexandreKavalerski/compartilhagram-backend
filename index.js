2817637d96b4419396dbd1d9d82efa4b20c6bee0
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2019-06-20 15:29:48 -0300

    rota de usuario adicionada

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const usuarios = require('./rotas/usuarios');
 
-app.get('', (req, res) => {
+
+app.get('/', (req, res) => {
     res.send('Olá mundo!');
 });
+
+app.use('/usuarios', usuarios);
 
 app.listen(3000, () => {
     console.log('API escutando na porta 3000');
