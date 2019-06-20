7e082d62cc9cb98091b5b7227ab6b2f68f69ea8d
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2019-06-20 18:11:04 -0300

    Separando configuração de upload de arquivo

--- rotas/usuarios.js
+++ rotas/usuarios.js
@@ -2,34 +2,7 @@ const express = require("express");
 const router = express.Router();
 const modelUsuario = require('../models/usuario');
 
-const multer = require('multer'), 
-    multers3 = require('multer-s3'),
-    path = require('path'),
-    AWS = require('aws-sdk');
-
-
-AWS.config.loadFromPath('s3_config.json');
-const s3 = new AWS.S3();
-
-
-const upload = multer({
-    storage: multers3({
-        s3: s3,
-        acl: 'public-read',
-        bucket: 'compartilhagram',
-        contentType: function(req, file, cb) {
-            cb(null, file.mimetype);
-        },
-        metadata: function (req, file, cb) {           
-            cb(null, { fieldName: file.fieldname, contentType: file.mimetype });
-        },
-        key: function (req, file, cb) {
-            cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
-        }
-    })
-});
-
-
+const upload = require('./upload');
 
 router.post('/autenticar', (req, res) => {
     dados = req.body;
