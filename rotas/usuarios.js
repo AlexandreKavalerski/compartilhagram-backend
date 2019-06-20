dffbd6a40aa81b8aac6ed472087be73b60e095ca
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2019-06-20 17:15:37 -0300

    Permitido upload de imagem de avatar (multer e s3)

--- rotas/usuarios.js
+++ rotas/usuarios.js
@@ -2,6 +2,34 @@ const express = require("express");
 const router = express.Router();
 const modelUsuario = require('../models/usuario');
 
+const multer = require('multer'), 
+    multers3 = require('multer-s3'),
+    path = require('path'),
+    AWS = require('aws-sdk');
+
+
+AWS.config.loadFromPath('s3_config.json');
+const s3 = new AWS.S3();
+
+
+const upload = multer({
+    storage: multers3({
+        s3: s3,
+        acl: 'public-read',
+        bucket: 'compartilhagram',
+        contentType: function(req, file, cb) {
+            cb(null, file.mimetype);
+        },
+        metadata: function (req, file, cb) {           
+            cb(null, { fieldName: file.fieldname, contentType: file.mimetype });
+        },
+        key: function (req, file, cb) {
+            cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+        }
+    })
+});
+
+
 
 router.get('/', (req, res) => {
     modelUsuario.find((err, usuarios)=>{
@@ -9,14 +37,13 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/cadastrar', (req, res) => {
+router.post('/cadastrar', upload.single('avatar'), (req, res) => {
     const dados = req.body;
-    const novoUsuario = new modelUsuario({nome: dados.nome, login: dados.login, avatar: dados.avatar});
+    const novoUsuario = new modelUsuario({nome: dados.nome, login: dados.login, avatar: req.file.location});
     novoUsuario.save();
 
     res.send({mensagem: "Novo usuário cadastrado no sistema", usuario: novoUsuario});
     
 });
-
 
 module.exports = router;
